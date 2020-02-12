7073f16933e6deee410b56b855cb2526fc90dc1a
ethLengfeld <elengfeld@wisc.edu>
2020-02-12 10:09:57 -0600

    added file headers

---
+++ BookDB.cpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////
+// Main File: BookDB.cpp, demo.cpp
+// This File: BookDB.cpp
+// Other Files: 	 BookDB.h / BookDB.cpp / demo.cpp / Makefile / README.md
+// Semester:         CS 368 Spring 2020
+//
+// Author:           Ethan Lengfeld
+// Email:            elengfeld@wisc.edu
+// CS Login:         lengfeld
+//
+//
+// Online sources:   
+//
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * TODO
+*/
+#include "BookDB.h"
