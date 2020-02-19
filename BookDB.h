8672639d896062517559339f33e13fd0fcaf6136
ethLengfeld <elengfeld@wisc.edu>
2020-02-18 18:54:29 -0600

    added function definitions

--- BookDB.h
+++ BookDB.h
@@ -17,3 +17,31 @@
  * TODO
 */
 // TODO define BookDB functions and Book struct
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef struct Book {
+    // TODO check what it wants write up says named ID
+    int named_ID;
+    int year;
+    double rating;
+} Book;
+
+vector<Book> books;
+
+void printBook();
+
+void addBook(int named_ID);
+
+void deleteBook(int named_ID);
+
+void findAndUpdateBook(int named_ID, int year, double rating);
+
+void calculateAvgBookRating();
+
+void printAllBooksOfYear(int year);
+
+void sortBooks(int sortOrder);
